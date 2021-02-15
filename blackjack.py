42be2514a1c2108821ef5764e54f4639f81b143f
p0nyo <78147374+p0nyo@users.noreply.github.com>
2021-02-15 18:38:52 +1300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -2,7 +2,7 @@ import random
 
 
 class Game:
-
+    # this sets the dealer and player's cards for the game
     def __init__(self):
         self.card1_player = random.randint(1, 11)
         self.card2_player = random.randint(1, 11)
@@ -11,6 +11,7 @@ class Game:
         self.card2_dealer = random.randint(1, 11)
         self.card_sum_dealer = self.card1_dealer + self.card2_dealer
 
+    # this function allows the player to either draw a card or not
     def draw(self):
         while True:
             if self.card_sum_player < 21:
@@ -24,6 +25,7 @@ class Game:
                 else:
                     print('Unknown input, please try again.')
 
+    # this function displays whether the player won or not
     def game_end(self, again_return):
         ending_code = {
             "2": "You Won!",
@@ -50,6 +52,12 @@ class Game:
             else:
                 print('Unknown input, please try again.')
 
+    # this function is simple and just prints score of both player and dealer
+    def score(self):
+        print('Cards: %d' % self.card_sum_player)
+        print("Dealer's Cards: %d" % self.card_sum_dealer)
+
+    # this function 'renews' the values of cards
     def renew(self):
         self.card1_player = random.randint(1, 11)
         self.card2_player = random.randint(1, 11)
@@ -58,35 +66,31 @@ class Game:
         self.card_sum_player = self.card1_player + self.card2_player
         self.card_sum_dealer = self.card1_dealer + self.card2_dealer
 
-    def score(self):
-        print('Cards: %d' % self.card_sum_player)
-        print("Dealer's Cards: %d" % self.card_sum_dealer)
-        
 
 def main():
     game = Game()
     while True:
         print('Cards: %d' % game.card_sum_player)
-        if game.draw() == 'stay':  #NOTE when stay is called
-            if game.card_sum_player > game.card_sum_dealer:
+        if game.draw() == 'stay':  # when stay is called from the game.draw() function
+            if game.card_sum_player > game.card_sum_dealer:  # the condition for winning after staying
                 game.score()
-                if game.game_end(2) == 'end':  #NOTE win
+                if game.game_end(2) == 'end':
                     break
-            elif game.card_sum_player < game.card_sum_dealer:
-                if game.game_end(1) == 'end':  #NOTE lose
+            elif game.card_sum_player < game.card_sum_dealer:  # the condition for losing after staying
+                if game.game_end(1) == 'end':
                     break
-            elif game.card_sum_player == game.card_sum_dealer:
-                if game.game_end(0) == 'end':  #NOTE draw
-                    break
-        if game.card_sum_player > 21:
-            game.score()
-            game.game_end(1)
-            if game.game_end(0) == 4:
+            elif game.card_sum_player == game.card_sum_dealer:  # the condition for a draw after staying
                 break
-        elif game.card_sum_player == 21:
+        if game.card_sum_player > 21:  # if exceeds 21 then lost
             game.score()
-            game.game_end(2)
-            if game.game_end(0) == 4:
+            if game.game_end(1) == 'end':
+                break
+        elif game.card_sum_player == 21:  # if is 21 then win
+            if game.card_sum_dealer == 21:  # unless dealer also gets 21
+                if game.game_end(0) == 'end':
+                    break
+            game.score()
+            if game.game_end(2) == 'end':
                 break
 
 
