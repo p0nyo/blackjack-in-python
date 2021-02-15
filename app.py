834c9f58a620ecb5d4a71f6f0fdea49e0648e470
p0nyo <78147374+p0nyo@users.noreply.github.com>
2021-02-15 18:15:09 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,94 @@
 import random
 
-player = 0
-dealer = 0
-deck = [
-    1, 1, 1, 1,
-    2, 2, 2, 2,
-    3, 3, 3, 3,
-    4, 4, 4, 4,
-    5, 5, 5, 5,
-    6, 6, 6, 6,
-    7, 7, 7, 7,
-    8, 8, 8, 8,
-    9, 9, 9, 9,
-    10, 10, 10, 10,
-    11, 11, 11, 11,
-    12, 12, 12, 12,
-    13, 13, 13, 13
-]
-while True:
-    question = input('Hit or Stay? ').lower()
-    if question == 'hit':
-        card_player = random.choice(deck)
-        card_dealer = random.choice(deck)
-        deck.remove(card_player)
-        player += card_player
-        if dealer < 17:
-            deck.remove(card_dealer)
-            dealer += card_dealer
-        elif 19 > dealer > 17:
-            dealer_choice = random.randint(1, 2)
-            if dealer_choice == 1:
-                deck.remove(card_dealer)
-                dealer += card_dealer
-                if dealer > 21:
-                    
-            elif dealer_choice == 2:
-                new_dealer = dealer
-                
-        print(f'You have {player}')
-        print(f'Dealer has {dealer}')
-        if player > 21:
-            print('You Lost!')
-            ans1 = input('Do you want to continue? (Y/N) ').lower()
-            if ans1 == 'y':
-                None
-            elif ans1 == 'n':
+
+class Game:
+
+    def __init__(self):
+        self.card1_player = random.randint(1, 11)
+        self.card2_player = random.randint(1, 11)
+        self.card_sum_player = self.card1_player + self.card2_player
+        self.card1_dealer = random.randint(1, 11)
+        self.card2_dealer = random.randint(1, 11)
+        self.card_sum_dealer = self.card1_dealer + self.card2_dealer
+
+    def draw(self):
+        while True:
+            if self.card_sum_player < 21:
+                question = input('Hit or Stay? ').lower()
+                if question == 'hit':
+                    self.card_sum_player += random.randint(1, 11)
+                    break
+                elif question == 'stay':
+                    return 'stay'
+                    break
+                else:
+                    print('Unknown input, please try again.')
+
+    def game_end(self, again_return):
+        ending_code = {
+            "2": "You Won!",
+            "1": "You Lost!",
+            "0": "Draw!"
+        }
+        if again_return == 2:
+            ending_input = ending_code['2']
+            print(ending_input)
+        elif again_return == 1:
+            ending_input = ending_code['1']
+            print(ending_input)
+        elif again_return == 0:
+            ending_input = ending_code['0']
+            print(ending_input)
+        while True:
+            message = input('Do you want to continue? (Y/N) ').lower()
+            if message == 'y':
+                self.renew()
+                break
+            elif message == 'n':
+                return 'end'
                 break
             else:
-                print('Error, please try again.')
-                None
-        elif player == 21 and player == dealer:
-            print('tie')
-    elif question == 'stay':
-        if player == 21:
-            print('You Won!')
-        elif player == 21 and player == dealer:
-            print('Tie')
-        elif player > dealer:
-            print('You Won!')
-        elif player == dealer:
-            print('Tie')
-        elif player < dealer:
-            print('You lost!')
-    else:
-        print('Error, please try again.')
+                print('Unknown input, please try again.')
 
+    def renew(self):
+        self.card1_player = random.randint(1, 11)
+        self.card2_player = random.randint(1, 11)
+        self.card1_dealer = random.randint(1, 11)
+        self.card2_dealer = random.randint(1, 11)
+        self.card_sum_player = self.card1_player + self.card2_player
+        self.card_sum_dealer = self.card1_dealer + self.card2_dealer
+
+    def score(self):
+        print('Cards: %d' % self.card_sum_player)
+        print("Dealer's Cards: %d" % self.card_sum_dealer)
+        
+
+def main():
+    game = Game()
+    while True:
+        print('Cards: %d' % game.card_sum_player)
+        if game.draw() == 'stay':  #NOTE when stay is called
+            if game.card_sum_player > game.card_sum_dealer:
+                game.score()
+                if game.game_end(2) == 'end':  #NOTE win
+                    break
+            elif game.card_sum_player < game.card_sum_dealer:
+                if game.game_end(1) == 'end':  #NOTE lose
+                    break
+            elif game.card_sum_player == game.card_sum_dealer:
+                if game.game_end(0) == 'end':  #NOTE draw
+                    break
+        if game.card_sum_player > 21:
+            game.score()
+            game.game_end(1)
+            if game.game_end(0) == 4:
+                break
+        elif game.card_sum_player == 21:
+            game.score()
+            game.game_end(2)
+            if game.game_end(0) == 4:
+                break
+
+
+if __name__ == '__main__':
+    main()
